3eb2eab5ec0065f94c7042a0492ca5bd110b5cbb
bleugreen <mitchell5799@gmail.com>
2026-08-10 00:32:31 -0400

    GTK 3 target: pin the Gdk introspection version

--- scripts/linux-toolkit-acceptance/targets/gtk3.py
+++ scripts/linux-toolkit-acceptance/targets/gtk3.py
@@ -7,6 +7,7 @@ import sys
 import gi
 
 gi.require_version("Gtk", "3.0")
+gi.require_version("Gdk", "3.0")
 gi.require_version("GdkX11", "3.0")
 from gi.repository import Gdk, GdkX11, GLib, Gtk  # noqa: E402
 
